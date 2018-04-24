c3b0a6841b5513357d0c3d1ec978455875938663
Jon Dunnett <dunnett_jonathan@wheatoncollege.edu>
2018-04-24 14:22:51 -0400

    Update barista_threading.py

--- barista_threading.py
+++ barista_threading.py
@@ -9,12 +9,6 @@ from datetime import datetime
 import ssl
 context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 context.load_cert_chain('certificate.pem', 'private-key.pem')
-'''
-from OpenSSL import SSL
-context = SSL.Context(SSL.SSLv23_METHOD)
-context.use_privatekey_file('private-key.pem')
-context.use_certificate_file('certificate.pem')
-'''
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.OUT)
@@ -129,4 +123,4 @@ def turn_off():
 	return statement('Coffee pot is off')
 
 if __name__ == '__main__':
-    app.run(host='155.47.187.6',port=443,ssl_context=context)
+    app.run(host='155.47.187.6',port=443)
